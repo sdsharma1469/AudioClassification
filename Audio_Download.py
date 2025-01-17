f7c5cab752ba73c1c90091b78c2a318072601dd7
sdsharma1469 <shaunak.sharma@gmail.com>
2025-01-16 20:50:10 -0800

    works

--- Audio_Download.py
+++ Audio_Download.py
@@ -1,4 +1,4 @@
 from audioset_download import Downloader
-d = Downloader(root_path='test/Siren', labels=["Siren",], n_jobs=5, download_type='unbalanced_train', copy_and_replicate=False)
+d = Downloader(root_path='test', labels=["Siren",], n_jobs=5, download_type='unbalanced_train', copy_and_replicate=False)
 
 d.download(format = 'vorbis')
